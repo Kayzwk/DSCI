a1971388d3145c3de673429e9723eb758648493b
Davi Silveira <105821905+Kayzwk@users.noreply.github.com>
2023-08-07 10:14:58 -0300

    Primeiro exercicio

---
+++ ex01.py
@@ -0,0 +1,12 @@
+nome = str(input('Digite seu nome: '));
+altura = str(input('Digite sua altura: '));
+peso = str(input('Digite seu peso: '));
+casado = False
+
+print(f'Nome: {nome}');
+print(f'Altura: {altura}');
+print(f'Peso: {peso}');
+if(casado == False)
+    print(f'Casado: Não');
+    else print(f'Casado: Sim');
+    
