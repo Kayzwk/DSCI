d46705d78eca9927de5cd0ea531533b26b5bf7a3
Kayzwk <davilego10@gmail.com>
2023-08-14 08:10:21 -0300

    Dois primeiros exercícios atualizados

---
+++ ex02.py
@@ -0,0 +1,11 @@
+nome = str(input('Digite seu nome: '));
+matricula = int(input('Digite sua matricula: '));
+campus = str(input('Digite seu campus: '));
+curso = str(input('Digite seu curso: '));
+anoDeEntrada = int(input('Digite o ano de entrada no curso: '));
+
+print(f'Nome: {nome}');
+print(f'Matricula: {matricula}');
+print(f'Campus: {campus}');
+print(f'Curso: {curso}');
+print(f'Ano de Entrada: {anoDeEntrada}');
