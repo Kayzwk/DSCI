ae082684ccf874aac9a89136e870b073efa141d5
Kayzwk <davilego10@gmail.com>
2023-09-25 09:55:37 -0300

    Novos exemplos adicionados

--- python/exemplos/getterProduto.py
+++ python/exemplos/getterProduto.py
@@ -3,7 +3,20 @@ class Produto:
         self.nome=nome
         self.tipo_unidade=tipo_unidade # lt, ml, kg, unidade
         self.quantidade=quantidade # 600 ml
-        self.preco=preco
+        self._preco=preco
+    
+    @property #getter
+    def preco(self):
+        return self._preco
+
+    @preco.setter
+    def preco(self, vl):
+        if isinstance(vl, str):
+            valor = vl.replace(' ','')
+            valor = float(valor.replace('R$',''))
+            self._preco=valor
+        else:
+            self._preco = vl
 
     def exibeProduto(self):
         print(f'Produto: {self.nome}')
@@ -11,7 +24,7 @@ class Produto:
         print(f'Valor: R$ {self.preco:.2f}')
 
     def venda(self,quantidade):
-        print(f'{self.nome} x {quantidade} = {self.preco*quantidade}')
+        print(f'{self.nome} x {quantidade} = R$ {self.preco*quantidade}')
 
 if __name__ == '__main__':
     prod1 = Produto('Coca-Cola','ml',600,6.00)
@@ -19,4 +32,4 @@ if __name__ == '__main__':
     print(type(prod1.preco))
     prod1.preco = 6.00
     prod1.exibeProduto()
-    prod1.venda(10)
+    prod1.venda(6)
